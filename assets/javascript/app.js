d582df5b185addb6b38c13b919233b6e2bfeff33
carlosalfonzo33 <carlos.alfonzo@gmail.com>
2017-06-06 18:05:27 -0700

    fixed the reset button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,216 +1,214 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-var questions = [
-{
-	question:  "What record-setting quarterback was the NFL's 82nd draft pick in 1979\?"+"<br/>",
-	choices: ["Jeff George", "Joe Montana", "Dan Marino","Bernie Kosar"],
-	correctAnswer: 1 
-}, 
-{
-	question: "Which superbowl was the most watched ever with 162\,900\,000\?"+"<br/>",
-	choices: ["XLV", "XL", "XXVII", "XXIV"],
-	correctAnswer: 0
+    var questions = [{
+            question: "What record-setting quarterback was the NFL's 82nd draft pick in 1979\?" + "<br/>",
+            choices: ["Jeff George", "Joe Montana", "Dan Marino", "Bernie Kosar"],
+            correctAnswer: 1
+        },
+        {
+            question: "Which superbowl was the most watched ever with 162\,900\,000\?" + "<br/>",
+            choices: ["XLV", "XL", "XXVII", "XXIV"],
+            correctAnswer: 0
 
-},
-{
-	question: "Which Green Bay Packer forced a key Rashard Mendenhall fumble in Super Bowl XLV\?"+"<br/>",
-	choices: ["BJ Raji", "Clay Matthews", "Charles Woodson","Reggie Wayne"],
-	correctAnswer: 1
+        },
+        {
+            question: "Which Green Bay Packer forced a key Rashard Mendenhall fumble in Super Bowl XLV\?" + "<br/>",
+            choices: ["BJ Raji", "Clay Matthews", "Charles Woodson", "Reggie Wayne"],
+            correctAnswer: 1
 
-},
-{
-	question: "This Team\, owned by Jerry Jones\, holds the record for most Super Bowl MVP\'s in the NFL"+"<br/>",
-	choices: ["Washington Redskins", "Dallas Cowboys", "Oakland Raiders", "New England Patriots"],
-	correctAnswer: 1
+        },
+        {
+            question: "This Team\, owned by Jerry Jones\, holds the record for most Super Bowl MVP\'s in the NFL" + "<br/>",
+            choices: ["Washington Redskins", "Dallas Cowboys", "Oakland Raiders", "New England Patriots"],
+            correctAnswer: 1
 
-},
-{
-	question: "Which receiver has the record for the most career Super Bowl receptions\?"+"<br/>",
-	choices: ["Deion Branch", "Andre Reed", "Jerry Rice", "Lynn Swann"],
-	correctAnswer: 2
+        },
+        {
+            question: "Which receiver has the record for the most career Super Bowl receptions\?" + "<br/>",
+            choices: ["Deion Branch", "Andre Reed", "Jerry Rice", "Lynn Swann"],
+            correctAnswer: 2
 
-},
-{
-	question: "Who performed during halftime of Superbowl XLII\?"+"<br/>",
-	choices: ["Elton John", "Rod Stewart", "Tom Petty", "Eric Clapton"],
-	correctAnswer: 2
+        },
+        {
+            question: "Who performed during halftime of Superbowl XLII\?" + "<br/>",
+            choices: ["Elton John", "Rod Stewart", "Tom Petty", "Eric Clapton"],
+            correctAnswer: 2
 
-},
-{
-	question: "Who holds the record for most rushing yards in a Super Bowl\?"+"<br/>",
-	choices: ["John Riggins", "Terrell Davis", "Timmy Smith", "Franco Harris"],
-	correctAnswer: 2
+        },
+        {
+            question: "Who holds the record for most rushing yards in a Super Bowl\?" + "<br/>",
+            choices: ["John Riggins", "Terrell Davis", "Timmy Smith", "Franco Harris"],
+            correctAnswer: 2
 
-},
-{
-	question:  "This defensive player holds the record for the most interceptions in a Super Bowl with 3\?"+"<br/>",
-	choices: ["Lester Hayes", "Rod Woodson", "Charles Woodson", "Rod Martin"],
-	correctAnswer: 3
+        },
+        {
+            question: "This defensive player holds the record for the most interceptions in a Super Bowl with 3\?" + "<br/>",
+            choices: ["Lester Hayes", "Rod Woodson", "Charles Woodson", "Rod Martin"],
+            correctAnswer: 3
 
-},
-{
-	question:  "Who caught the game-winning touchdown for the New York Giants in Super Bowl XLII\?"+"<br/>",
-	choices: ["David Tyree", "Plaxico Burress", "Kevin Boss","Steve Smith"],
-	correctAnswer: 1
+        },
+        {
+            question: "Who caught the game-winning touchdown for the New York Giants in Super Bowl XLII\?" + "<br/>",
+            choices: ["David Tyree", "Plaxico Burress", "Kevin Boss", "Steve Smith"],
+            correctAnswer: 1
 
-},
-{
-	question:  "This quaterback has been named Super Bowl MVP a record 4 times?"+"<br/>",
-	choices: ["Joe Montana", "Terry Bradshaw", "Tom Brady", "Bart Starr"],
-	correctAnswer: 2
+        },
+        {
+            question: "This quaterback has been named Super Bowl MVP a record 4 times?" + "<br/>",
+            choices: ["Joe Montana", "Terry Bradshaw", "Tom Brady", "Bart Starr"],
+            correctAnswer: 2
 
-}
+        }
 
-];
-
-
-//questions
- $.each(questions, function(index,value){
- 	console.log(questions);
-
- 		var $li=$("<li></li>");
- 		$li.html(index + 1+". "+value.question);
- 		$li.attr("correctAnswer", value.correctAnswer)
- 		//console.log("correctAnswer");
-
-//choices
- 		$.each(value.choices, function(choiceIndex, choice){
- 			//console.log(value);
+    ];
 
 
- 			var $input=$("<input/>");
-                $input.attr("type", "radio");
-                $input.attr("name","question"+index);
-                $input.attr("value", choice);
-                $input.attr("id", choice)
-                $li.append($input);
-                //console.log($input);
+    //questions
+    $.each(questions, function(index, value) {
+        console.log(questions);
+
+        var $li = $("<li></li>");
+        $li.html(index + 1 + ". " + value.question);
+        $li.attr("correctAnswer", value.correctAnswer)
+        //console.log("correctAnswer");
+
+        //choices
+        $.each(value.choices, function(choiceIndex, choice) {
+            //console.log(value);
+
+
+            var $input = $("<input/>");
+            $input.attr("type", "radio");
+            $input.attr("name", "question" + index);
+            $input.attr("value", choice);
+            $input.attr("id", choice)
+            $li.append($input);
+            //console.log($input);
 
             var label = $("<label/>")
-                label.attr("for", choice);
-                label.text(choice);
-                $li.append(label);
+            label.attr("for", choice);
+            label.text(choice);
+            $li.append(label);
 
- 		})
+        })
 
-			$("#quiz-question").append($li);
-			//console.log("$li");
+        $("#quiz-question").append($li);
+        //console.log("$li");
 
- });
-
-
-
-var answerCorrect = 0;
-var answerWrong = 0;
-var notAnswered = 0;
-var timer;
-
-// var countDown = function() {
-// 	var counts = 60;
-//     $("#count").html(counts--);
-//     if(counts === 0) {
-//     	console.log('time is up!!!');
-//     	$("#quiz-question").hide();
-//     	$("#startClock").html('Time is up!!');
-//     }
-//     if(counts <= -1) {
-//     	clearInterval(timer);
-//     }
-// };
+    });
 
 
-function startTimer(count) {
-	// var counts = 60;
-	console.log(count);
-	timer = setInterval(function() {
-	
-    $("#count").html(count--);
-    if(count === 0) {
-    	console.log('time is up!!!');
-    	$("#quiz-question").hide();
-    	$("#startClock").html('Time is up!!');
-    }
-    if(count <= -1) {
-    	clearInterval(timer);
-    }
-}, 1000);    
-};
 
-$('#start').click(function() {
-	$(".blanket").removeClass("hidden");
-	$(this).addClass("hidden");
-	startTimer(60);
-	  	
-});
+    var answerCorrect = 0;
+    var answerWrong = 0;
+    var notAnswered = 0;
+    var timer;
 
-function questChecker() {
-	$("input:checked").each(function(i, input) {
-		var val = $(input).val();
+    // var countDown = function() {
+    // 	var counts = 60;
+    //     $("#count").html(counts--);
+    //     if(counts === 0) {
+    //     	console.log('time is up!!!');
+    //     	$("#quiz-question").hide();
+    //     	$("#startClock").html('Time is up!!');
+    //     }
+    //     if(counts <= -1) {
+    //     	clearInterval(timer);
+    //     }
+    // };
+
+
+    function startTimer(count) {
+        // var counts = 60;
+        console.log(count);
+        timer = setInterval(function() {
+
+            $("#count").html(count--);
+            if (count === 0) {
+                console.log('time is up!!!');
+                $("#quiz-question").hide();
+                $("#startClock").html('Time is up!!');
+            }
+            if (count <= -1) {
+                clearInterval(timer);
+            }
+        }, 1000);
+    };
+
+    $('#start').click(function() {
+        $(".blanket").removeClass("hidden");
+        $(this).addClass("hidden");
+        startTimer(60);
+
+    });
+
+    function questChecker() {
+        $("input:checked").each(function(i, input) {
+            var val = $(input).val();
             //console.log("i", i);
             // console.log("input", input);
             // console.log("val", val);
-         var answerIndex = $(input).parent().attr("correctAnswer");
+            var answerIndex = $(input).parent().attr("correctAnswer");
             //console.log("answer", answer);
-         console.log("correct answer: ", questions[i].choices[answerIndex]);
-          var answer = questions[i].choices[answerIndex];
-            
-          if(val == answer){
+            console.log("correct answer: ", questions[i].choices[answerIndex]);
+            var answer = questions[i].choices[answerIndex];
+
+            if (val == answer) {
                 answerCorrect++;
+            } else {
+                answerWrong++;
             }
- 
-            else {
-                answerWrong++;            
-            }
-           
-            
-     });
-     notAnswered = questions.length - (answerCorrect + answerWrong);
-}
 
-$("#results").hide();
+
+        });
+        notAnswered = questions.length - (answerCorrect + answerWrong);
+    }
+
+    $("#results").hide();
 
 
 
-// Done button function to display results
+    // Done button function to display results
 
-$("#submit").on("click", function() {
+    $("#submit").on("click", function() {
 
-	questChecker();
-	$("#results").show();
-	$("#correctAns").text(answerCorrect);
-	$("#wrongAns").text(answerWrong);
-	$("#notAns").text(notAnswered);
-	$("#quiz-question").hide();
-	$("#submit").hide();
-	$("#count").hide();
-	$("#button-restart").show();
-	clearInterval(timer);
+        questChecker();
+        $("#results").show();
+        $("#correctAns").text(answerCorrect);
+        $("#wrongAns").text(answerWrong);
+        $("#notAns").text(notAnswered);
+        $("#quiz-question").hide();
+        $("#submit").hide();
+        $("#count").hide();
+        $("#button-restart").show();
+        clearInterval(timer);
+
+    });
+    var spawnRestartButton = function() {
+        var button = $("<button>").text("Restart");
+        button.hide();
+        button.attr("id", "button-restart");
+        $(".question-container").append(button);
+
+    }
+
+    spawnRestartButton();
+
+    //Restarts the time after restart button is pressed
+
+	$(document).on("click", "#button-restart", function() {
+	    $(".question-container").empty();
+	    $("#quiz-question").show();
+	    $("#results").hide()
+	    $("#submit").show();
+	    $("#count").show();
+	    startTimer(60)
+
 
 	});
-var spawnRestartButton = function() {
-	var button = $("<button>").text("Restart");
-	button.hide();
-	button.attr("id", "button-restart");
-	$(".question-container").append(button);
 
-}
+	$("#song")[0].play();
 
-spawnRestartButton();
-// #("#button-restart").on("click")
+	$("#song")[0].loop = true;
 
 });
-
-$(document).on("click", "#button-restart", function() {
-	$(".question-container").empty();
-	$("#quiz-question").show();
-	$("#results").hide()
-	$("#submit").show();
-	$("#count").show();
-	startTimer(60);
-		
-
-});
-
-$("#song")[0].play();
-
-$("#song")[0].loop = true;
